3bf7dfdc5d1fe7a81cf6b0e2cef1038c35396b43
Ethan James Beal <21134925+HDeDeDe@users.noreply.github.com>
2024-10-20 15:54:24 -0700

    update version number

--- HealthComponentAPI/HealthComponentAPI.cs
+++ HealthComponentAPI/HealthComponentAPI.cs
@@ -10,7 +10,7 @@ namespace HDeMods {
 		public const string PluginGUID = PluginAuthor + "." + PluginName;
 		public const string PluginAuthor = "HDeDeDe";
 		public const string PluginName = "HealthComponentAPI";
-		public const string PluginVersion = "0.1.1";
+		public const string PluginVersion = "0.2.0";
 
 		private static UpdateHealthEventArgs HealthStats;
 		private static HealEventArgs HealStats;
