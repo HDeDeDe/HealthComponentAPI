e3688fd51fbe5c125329e3e5834bde65727d8c7c
Ethan James Beal <21134925+HDeDeDe@users.noreply.github.com>
2024-10-20 15:55:26 -0700

    redid every hook

--- HealthComponentAPI/HealthComponentAPI.cs
+++ HealthComponentAPI/HealthComponentAPI.cs
@@ -165,38 +165,42 @@ namespace HDeMods {
 		}
 
 		private static void RecalcCoyoteTimer(ILCursor c) {
-			c.GotoNext(
-				x => x.MatchLdarg(0),
-				x => x.MatchLdcR4(0.2f),
-				// Inserting here
-				x => x.MatchStfld<HealthComponent>("recentlyTookDamageCoyoteTimer")
-			);
-			c.Index += 2;
+			if (!c.TryGotoNext(
+				moveType: MoveType.Before,
+                x => x.MatchStfld<HealthComponent>("recentlyTookDamageCoyoteTimer")
+			    )) {
+				Log.Fatal("Failed to hook Coyote Timer!");
+				return;
+			}
 			c.EmitDelegate<RuntimeILReferenceBag.FastDelegateInvokers.Func<float, float>>((consume) => 
 				RefVal.damageCoyoteTimer * (1f + HealStats.damageCoyoteTimerMultAdd) + HealStats.damageCoyoteTimerFlatAdd);
 		}
 
 		private static void RecalcCritHeal(ILCursor c) {
-			c.GotoNext(
-				x => x.MatchLdarg(1),
-				x => x.MatchLdcR4(2f),
-				// Inserting here
-				x => x.MatchMul(),
-				x => x.MatchStarg(1)
-			);
-			c.Index += 2;
+			if (!c.TryGotoNext(
+				    x => x.MatchLdloc(1),
+				    x=> x.MatchBrfalse(out _),
+				    x => x.MatchLdarg(1),
+				    x => x.MatchLdcR4(out _),
+				    // Inserting Here
+				    x => x.MatchMul()
+			    )) {
+				Log.Fatal("Failed to hook Crit Heal!");
+				return;
+			}
+			c.Index += 4;
 			c.EmitDelegate<RuntimeILReferenceBag.FastDelegateInvokers.Func<float, float>>((consume) => 
 				RefVal.critHealMultiplier * (1f + HealStats.critHealMultAdd) + HealStats.critHealFlatAdd);
 		}
 
 		private static void HalveHealing(ILCursor c) {
-			c.GotoNext(
-				x => x.MatchCall<Run>("get_instance"),
-				x => x.MatchCallvirt<Run>("get_selectedDifficulty"),
-				// Inserting here
-				x => x.MatchLdcI4(7)
-			);
-			c.Index += 2;
+			if (!c.TryGotoNext(
+				    moveType: MoveType.After,
+				    x => x.MatchCallvirt<Run>("get_selectedDifficulty")
+			    )) {
+				Log.Fatal("Failed to hook Eclipse 5 healing!");
+				return;
+			}
 			c.EmitDelegate<RuntimeILReferenceBag.FastDelegateInvokers.Func<int, int>>(consume => {
 				if (!HealStats.enableEclipseHealReduction) return consume;
 				return (int)RefVal.e5;
@@ -204,15 +208,18 @@ namespace HDeMods {
 		}
 
 		private static void RecalcTOTALHeal(ILCursor c) {
-			c.GotoNext(
-				x => x.MatchLdarg(1),
-				x => x.MatchLdloc(4),
-				x => x.MatchMul(),
-				x => x.MatchStarg(1),
-				x => x.MatchLdarg(1),
-				// Inserting here
-				x => x.MatchStloc(2)
-			);
+			if (!c.TryGotoNext(
+				    x => x.MatchLdarg(1),
+				    x => x.MatchLdloc(4),
+				    x => x.MatchMul(),
+				    x => x.MatchStarg(1),
+				    x => x.MatchLdarg(1),
+				    // Inserting here
+				    x => x.MatchStloc(2)
+			    )) {
+				Log.Fatal("Failed to hook TOTAL Healing!");
+				return;
+			}
 			c.Index += 5;
 			c.EmitDelegate<RuntimeILReferenceBag.FastDelegateInvokers.Func<float, float>>((TOTALHeal) => 
 				TOTALHeal * (1f + HealStats.TOTALhealAmountMultAdd) + HealStats.TOTALhealAmountFlatAdd);
@@ -220,63 +227,52 @@ namespace HDeMods {
 			c.Emit(OpCodes.Ldarg_1);
 		}
 
-		private static void RecalcTOTALRegenAcumulator(ILCursor c) {
-			c.GotoNext(
-				x => x.MatchLdarg(0),
-				x => x.MatchLdfld<HealthComponent>("regenAccumulator"),
-				x => x.MatchLdarg(0),
-				x => x.MatchLdfld<HealthComponent>("body"),
-				x => x.MatchCallvirt<CharacterBody>("get_regen"),
-				// Insert Here
-				x => x.MatchLdarg(1),
-				x => x.MatchMul()
-			);
-			c.Index += 5;
 		private static void RecalcTOTALRegenAccumulator(ILCursor c) {
+			if (!c.TryGotoNext(
+				    moveType: MoveType.After,
+				    x => x.MatchCallvirt<CharacterBody>("get_regen")
+			    )) {
+				Log.Fatal("Failed to hook Regen Accumulator!");
+				return;
+			}
 			c.EmitDelegate<RuntimeILReferenceBag.FastDelegateInvokers.Func<float, float>>((regen) => 
 				regen * (1f + HealthStats.TOTALregenMultAdd) + HealthStats.TOTALregenFlatAdd);
 		}
 		
 		private static void RecalcBarrierDecayRate(ILCursor c) {
-			c.GotoNext(
-				x => x.MatchLdarg(0),
-				x => x.MatchLdfld<HealthComponent>("barrier"),
-				x => x.MatchLdarg(0),
-				x => x.MatchLdfld<HealthComponent>("body"),
-				x => x.MatchCallvirt<CharacterBody>("get_barrierDecayRate")
-				// Inserting here
-			);
-			c.Index += 5;
+			if (!c.TryGotoNext(
+				    moveType: MoveType.After,
+				    x => x.MatchCallvirt<CharacterBody>("get_barrierDecayRate")
+			    )) {
+				Log.Fatal("Failed to hook Barrier Decay Rate!");
+				return;
+			}
 			c.Emit(OpCodes.Ldarg_0);
 			c.EmitDelegate<RuntimeILReferenceBag.FastDelegateInvokers.Func<float, HealthComponent, float>>((consume, hc) => hc.body.maxBarrier / (RefVal.barrierDecayRate * (1f + HealthStats.barrierDecayRateMultAdd) + HealthStats.barrierDecayRateFlatAdd));
 		}
 
 		private static void RecalcShieldRechargeRate(ILCursor c) {
-			c.GotoNext(
-				x => x.MatchLdloc(4),
-				x => x.MatchLdarg(0),
-				x => x.MatchLdfld<HealthComponent>("body"),
-				x => x.MatchCallvirt<CharacterBody>("get_maxShield"),
-				x => x.MatchLdcR4(0.5f),
-				// Inserting here
-				x => x.MatchMul()
-			);
-			c.Index += 5;
+			if (!c.TryGotoNext(
+				    moveType: MoveType.After,
+				    x => x.MatchCallvirt<CharacterBody>("get_maxShield"),
+				    x => x.MatchLdcR4(out _)
+			    )) {
+				Log.Fatal("Failed to hook Shield Recharge Rate!");
+				return;
+			}
 			c.EmitDelegate<RuntimeILReferenceBag.FastDelegateInvokers.Func<float, float>>((consume) => 
 				RefVal.shieldRechargeRate * (1 + HealthStats.shieldRechargeRateMultAdd) + HealthStats.shieldRechargeRateFlatAdd);
 		}
 
 		private static void RecalcAdaptiveArmorDecayRate(ILCursor c) {
-			c.GotoNext(
-				x => x.MatchLdarg(0),
-				x => x.MatchLdcR4(0),
-				x => x.MatchLdarg(0),
-				x => x.MatchLdfld<HealthComponent>("adaptiveArmorValue"),
-				x => x.MatchLdcR4(40),
-				// Inserting Here
-				x => x.MatchLdarg(1)
-			);
-			c.Index += 5;
+			if (!c.TryGotoNext(
+				    moveType: MoveType.After,
+					x => x.MatchLdfld<HealthComponent>("adaptiveArmorValue"),
+				    x => x.MatchLdcR4(out _)
+			    )) {
+				Log.Fatal("Failed to hook Adaptive Armor Decay Rate!");
+				return;
+			}
 			c.EmitDelegate<RuntimeILReferenceBag.FastDelegateInvokers.Func<float, float>>((consume) => 
 				RefVal.adaptiveArmorDecayRate * (1 + HealthStats.adaptiveArmorDecayRateMultAdd) + HealthStats.adaptiveArmorDecayRateFlatAdd);
 		}
